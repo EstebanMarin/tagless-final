39c57e9e4c649800d660dd4a9c26b7ffa526637f
EstebanMarin <estebanmarin002@gmail.com>
2022-11-10 09:35:27 -0500

    Step 0: Foundations

---
+++ README.md
@@ -0,0 +1,11 @@
+# A sensible approach to Tagless final
+## Motivation
+
+Bases in the storyline that (FP fundamentals)[https://github.com/47degrees/fp-fundamentals] creates. 
+
+# Tagless Final
+- (Step 0: Fundations)[] What is this expression problem
+
+### More on Tagless          
+    - (Dev Inside you)[https://github.com/DevInsideYou/tagless-final]
+    -  (RJVM)[https://blog.rockthejvm.com/tagless-final/]
--- src/main/scala/example/TaglessFinal.scala
+++ src/main/scala/example/TaglessFinal.scala
@@ -1,3 +1,26 @@
 package example
 
-object Tagless extends App {}
+object Tagless {
+  object expressionProblem {
+    // how to add code without recompiling exiting code
+    trait Expr
+    case class Lit(b: Boolean) extends Expr
+    case class Or(left: Expr, right: Expr) extends Expr
+    case class And(left: Expr, right: Expr) extends Expr
+    case class Not(expr: Expr) extends Expr
+  }
+
+  object Programs {
+    import expressionProblem._
+
+    val expression1 = And(
+      Lit(true),
+      Or(Lit(false), Lit(true))
+    ) // true && (false || true) => true
+
+  }
+
+
+
+  def main(args: Array[String]) = println("Hello")
+}
