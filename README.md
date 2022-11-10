81a9392fab528248dd3106b09ac2d1e7aea86756
EstebanMarin <estebanmarin002@gmail.com>
2022-11-10 10:19:32 -0500

    Step 1: Understanding the problem

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 Bases in the storyline that (FP fundamentals)[https://github.com/47degrees/fp-fundamentals] creates. 
 
 # Tagless Final
-- (Step 0: Fundations)[] What is this expression problem
+- (Step 0: Fundations)[] Expression in functional programming
+- (Step 1: Understanding the problem)[] What is this expression problem
 
 ### More on Tagless          
     - (Dev Inside you)[https://github.com/DevInsideYou/tagless-final]
--- src/main/scala/example/TaglessFinal.scala
+++ src/main/scala/example/TaglessFinal.scala
@@ -2,6 +2,7 @@ package example
 
 object Tagless {
   object expressionProblem {
+    // No access to this code => Bonus setup an SBT separate and do a compile version of the problem
     // how to add code without recompiling exiting code
     trait Expr
     case class Lit(b: Boolean) extends Expr
@@ -20,7 +21,32 @@ object Tagless {
 
   }
 
+  object Interpreter {
+    import expressionProblem._
 
+    def eval(expr: Expr): Boolean = expr match {
+      case And(left, right) => eval(left) && eval(right)
+      case Lit(b)           => b
+      case Not(expr)        => !eval(expr)
+      case Or(left, right)  => eval(left) || eval(right)
+    }
+  }
+
+  object Orchestrator {
+    import expressionProblem._
+    import Programs.expression1
+
+    val value: Boolean = Interpreter.eval(expression1)
+    // Intro to the problem => Extending to other Types the expression
+    case class I(int: Int) extends Expr
+    case class Sum(l: Expr, r: Expr) extends Expr
+    // I am even able to create describe the computation
+    val expression2: Sum = Sum(I(9), I(8))
+    // Our clases will not be interpreted by library.
+    // Library has to change the approach
+    val value2: Boolean = Interpreter.eval(expression2)
+
+  }
 
   def main(args: Array[String]) = println("Hello")
 }
